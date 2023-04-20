57e86c2cfbf83982133ff731a95e6bbdb62efb09
tejasbahiram123 <tejasbahiram12@gmail.com>
2023-04-20 18:38:49 +0530

    fiest commit by tejas

---
+++ src/main/java/com/client/ClientApplication.java
@@ -0,0 +1,20 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.addition.calculator.Sum;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+		
+		Sum sum = new Sum();
+		
+		int sum2 = sum.sum(10, 30);
+		System.out.println("This result from client application"+sum2);
+	}
+
+}
